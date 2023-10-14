a44acbce58dc578f04c4701a389982cbe53b8301
Sulfurous-Impersonation <june@watson-net.com>
2023-10-13 21:30:37 -0700

    Initial player movement, doesn't work

--- levels/gridmap.gd
+++ levels/gridmap.gd
@@ -10,10 +10,18 @@ func get_grid():
 	print("grid with columns: ", grid)
 	return grid
 
+# get the positions of child nodes, as well as their visibility
+# visibility is used as 4th dimension of Vector4 to determine if the given node
+# can be occupied or not
 func get_child_positions(node):
+	# get children and initialize array of positions
 	var children = node.get_children()
 	var positions = []
+	
+	# for each element of children, append a new Vector4(global_position(x,y,z), visibility)
+	# to positions
 	for i in range(len(children)):
-		positions.append(children[i].get_global_position())
+		var v3 = children[i].get_global_position()
+		positions.append(Vector4(v3.x, v3.y, v3.z, float(children[i].visible)))
 	print("columns: ", positions)
 	return positions
