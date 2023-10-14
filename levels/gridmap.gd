56aa4187cb27c99905f626567bc403ef3d40b68f
Sulfurous-Impersonation <june@watson-net.com>
2023-10-13 20:42:03 -0700

    Added grid stuff!

--- levels/gridmap.gd
+++ levels/gridmap.gd
@@ -1,7 +1,19 @@
 extends GridMap
 
-@export var grid_array : Array[Vector3]
-
 # Return a duplicate of the grid
 func get_grid():
-	return grid_array.duplicate()
+	var children = get_children()
+	var grid = []
+	for i in range(len(children)):
+		print("getting columns")
+		grid.append(get_child_positions(children[i]))
+	print("grid with columns: ", grid)
+	return grid
+
+func get_child_positions(node):
+	var children = node.get_children()
+	var positions = []
+	for i in range(len(children)):
+		positions.append(children[i].get_global_position())
+	print("columns: ", positions)
+	return positions
